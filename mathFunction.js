2cdbc1122989615fa7dbeaef39041badf5cda548
Suwan KHP <63299775+oncepk@users.noreply.github.com>
2023-03-13 16:22:47 +0700

    mathFunction

---
+++ mathFunction.js
@@ -0,0 +1,12 @@
+let a = Math.random();
+let b = Math.random();
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
